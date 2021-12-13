c52041240d9befbf808e0fe9f378b1098e4ffe4f
Lepisok <aleksandr_podkop@mail.ru>
2021-12-13 23:47:54 +0300

    update_lesson_2

--- geekshop/views.py
+++ geekshop/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'geekshop/index.html')
+    title = 'GeekShop'
+    context = {
+        'title': title,
+    }
+    return render(request, 'geekshop/index.html', context)
+
 
 def contacts(request):
-    return render(request, 'geekshop/contacts.html')
+    title = 'Contacts'
+    context = {
+        'title': title,
+    }
+    return render(request, 'geekshop/contacts.html', context)
