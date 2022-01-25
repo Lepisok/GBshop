b34a05d294db18601212dcd300290380838cf038
Lepisok <aleksandr_podkop@mail.ru>
2022-01-25 16:55:50 +0300

    add_lesson_2_4

--- basketapp/static/basketapp/js/basket_scripts.js
+++ basketapp/static/basketapp/js/basket_scripts.js
@@ -1,13 +1,14 @@
 window.onload = function () {
-    $('.basket_list').on(types: 'click', selector: 'input[type="number"]', data: function (){
+    $('.basket_list').on('click', 'input[type="number"]', function () {
         var t_href = event.target;
 
-        $.ajax(url:{
-            url: "/basket/edit" + t_href.name + "/" + t_href.value + "/",
+        $.ajax({
+            url: "/basket/edit/" + t_href.name + "/" + t_href.value + "/",
             success: function (data) {
                 $('.basket_list').html(data.result);
-            };
+            },
+
         });
-        event.prevent
+        event.preventDefault();
     });
 }
